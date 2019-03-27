1debd5d8d854839aa3fbff2fc7909b6dbb90f274
Administrator <Administrator@PC036>
2019-03-27 16:21:59 +0700

    Add project files.

---
+++ laptrinhweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(laptrinhweb.Startup))]
+namespace laptrinhweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ laptrinhweb/ViewModels/CourseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using laptrinhweb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace laptrinhweb.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+    }
+}
